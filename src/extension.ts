cc0877ae51da21d4416faaafc18b95fe741b7832
Danny Tuppeny <danny@tuppeny.com>
2025-06-30 11:55:58 +0100

    Implement WS connection and custom delay

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as http from 'http';
 import * as vscode from 'vscode';
 import * as WebSocket from 'ws';
 
-export async function setUpServers(context: vscode.ExtensionContext) {
+export async function setUpServers(context: vscode.ExtensionContext, delay: number) {
 	const htmlServers: http.Server[] = [];
 	const wsServers: Array<{ close: () => void }> = [];
 	const bgColors = ['red', 'blue', 'green'];
@@ -14,15 +14,42 @@ export async function setUpServers(context: vscode.ExtensionContext) {
 		const wsHttpServer = http.createServer();
 		const wsServer = new WebSocket.Server({ server: wsHttpServer });
 		wsServer.on('connection', (socket: WebSocket.WebSocket) => {
-			socket.send('Hello World');
+			socket.send('Hello from WebSocket Server ${i}');
 		});
 		wsHttpServer.listen();
 		wsServers.push(wsHttpServer);
 		wsServers.push(wsServer);
 
+		// Get the URI for the WebSocket.
+		const wsPort = (wsHttpServer.address() as any).port;
+		const wsUri = await vscode.env.asExternalUri(vscode.Uri.parse(`http://localhost:${wsPort}`));
+
 		const htmlServer = http.createServer((req, res) => {
 			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end(`<html><body bgcolor="${bgColors[i]}"><h1 style="font-family: sans-serif">Server ${i}</h1></body></html>`);
+			res.end(`
+				<html>
+				<body bgcolor="${bgColors[i]}" style="font-family: sans-serif">
+					<h1>Server ${i}</h1>
+					<script>
+						function append(s) {
+							const msg = document.createElement('p');
+							msg.textContent = s;
+							document.body.appendChild(msg);
+						}
+						const ws = new WebSocket('${wsUri}');
+						ws.onmessage = function(event) {
+							append(event.data);
+						};
+						ws.onopen = function() {
+							append('WebSocket connected');
+						};
+						ws.onerror = function(e) {
+							append(\`WebSocket error: \${e}\`);
+						};
+					</script>
+				</body>
+				</html>
+			`);
 		});
 		htmlServer.listen();
 		htmlServers.push(htmlServer);
@@ -36,8 +63,8 @@ export async function setUpServers(context: vscode.ExtensionContext) {
 		res.end(`
 				<!DOCTYPE html>
 				<html><body bgcolor="yellow">
-					<h1 style="font-family: sans-serif">Main Server</h1>
-					${frameUris.map((uri) => `<iframe src="${uri}" width="500" height="100"></iframe><br/>`).join("\n")}
+					<h1 style="font-family: sans-serif">Main Server (${delay}ms delay)</h1>
+					${frameUris.map((uri) => `<iframe src="${uri}" width="500" height="150"></iframe><br/>`).join("\n")}
 				</body></html>
 			`);
 	});
@@ -60,7 +87,7 @@ export async function setUpServers(context: vscode.ExtensionContext) {
 		<html>
 		<body>
 		<h1 style="font-family: sans-serif">Panel HTML</h1>
-		<iframe id="x" src="${mainUri}" width="600" height="500"></iframe>
+		<iframe id="x" src="${mainUri}" width="600" height="750"></iframe>
 		</body>
 		</html>
 	`;
@@ -74,6 +101,10 @@ export async function setUpServers(context: vscode.ExtensionContext) {
 }
 
 export async function activate(context: vscode.ExtensionContext) {
-	await new Promise((resolve) => setTimeout(resolve, 1000));
-	await setUpServers(context);
+	const delay = vscode.workspace.getConfiguration('portTest').get<number>('delay', 0);
+
+	if (delay) {
+		await new Promise((resolve) => setTimeout(resolve, delay));
+	}
+	await setUpServers(context, delay);
 }
